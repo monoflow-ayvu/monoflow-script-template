d5c3425e833bbadb17c952148896bf9aa9f2b923
Fernando Mumbach <fermuch@ayvu.net>
2021-08-23 14:28:24 -0300

    fix: siempre enviando id en colecciones

--- src/vamos_logic.ts
+++ src/vamos_logic.ts
@@ -34,25 +34,19 @@ export default function install() {
 
   // check "Frota" collection
   const frotaCol = env.project?.collectionsManager.ensureExists<FrotaCollection>("frota");
-  if (!frotaCol.has(deviceId)) {
-    frotaCol.set(`${deviceId}.id`, deviceId);
-  }
+  frotaCol.set(`${deviceId}.id`, deviceId);
   frotaCol.set(`${deviceId}.scriptVer`, SCRIPT_VER);
   frotaCol.set(`${deviceId}.appVer`, data.APP_VERSION || 'unknown');
 
   // check "BLE" collection
   const bleCol = env.project?.collectionsManager.ensureExists<BleCollection>("ble");
-  if (!bleCol.has(deviceId)) {
-    bleCol.set(`${deviceId}.id`, deviceId);
-  }
+  bleCol.set(`${deviceId}.id`, deviceId);
   const foundBle = bleCol.typedStore[deviceId].target || '';
   data.BLE_TARGET = foundBle;
 
   // check "metrics" collection
   const metricsCol = env.project?.collectionsManager.ensureExists<MetricsCollection>('metrics');
-  if (!metricsCol.has(deviceId)) {
-    metricsCol.set(`${deviceId}.id`, deviceId);
-  }
+  metricsCol.set(`${deviceId}.id`, deviceId);
   metricsCol.bump(`${deviceId}.bootTimes`, 1);
 
   // subscribe to events
